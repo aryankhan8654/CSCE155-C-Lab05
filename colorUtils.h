724b5f50c353fd101de34b7e3a5e88b8e8ea23c4
aryan <aryankhan8654272@gmail.com>
2023-12-19 22:08:22 +0500

    first commit

--- colorUtils.h
+++ colorUtils.h
@@ -1,28 +1,37 @@
-/**
- * A collection of utility functions to manipualte RGB
- * values of an image
- */
+#include <stdio.h>
+#include <math.h>
 
-/**
- * Scales the given rgb integer component (on the
- * scale [0, 255]) to a floating point number on the
- * scale [0, 1].
- *
- * Note: this function is only included to demonstrate
- * how to unit test floating point values.  See the
- * colorUtilsTester.c for more.
- */
-double rgbIntToFloat(int c);
+// Scales the given rgb integer component (on the
+// scale [0, 255]) to a floating point number on the
+// scale [0, 1].
+//
+// Note: this function is only included to demonstrate
+// how to unit test floating point values. See the
+// colorUtilsTester.c for more.
+double rgbIntToFloat(int c)
+{
+    return (double)c / 255.0;
+}
 
-/**
- * Returns the maximum value among the three
- * given integer values.
- */
-int max(int x, int y, int z);
+// Returns the maximum value among the three
+// given integer values.
+int max(int x, int y, int z)
+{
+    return x > y ? (x > z ? x : z) : (y > z ? y : z);
+}
 
-/**
- * Returns a gray-scaled RGB value of the three
- * given RGB values using the average technique.
- *
- */
-int toGrayScaleAverage(int r, int g, int b);
+// Returns a gray-scaled RGB value of the three
+// given RGB values using the average technique.
+//
+int toGrayScaleAverage(int r, int g, int b)
+{
+    int avg = (r + g + b) / 3;
+    return avg << 16 | avg << 8 | avg;
+}
+
+int main()
+{
+    int r = 255, g = 128, b = 64;
+    printf("Gray-scaled RGB: %06X\n", toGrayScaleAverage(r, g, b));
+    return 0;
+}
